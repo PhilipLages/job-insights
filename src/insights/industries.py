0997029dfd9cca99a1d2b9bc1dfc5843dd661f7b
Philip Lages <psilvaelages@gmail.com>
2023-03-24 16:16:43 -0300

    feat: Implements get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from .jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,12 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    data = read(path)
+    industries = set()
+    for row in data:
+        if row["industry"]:
+            industries.add(row["industry"])
+    return list(industries)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
