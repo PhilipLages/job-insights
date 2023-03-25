2dc6cad8aac273f9507e95e28eb19f78b57c05de
Philip Lages <psilvaelages@gmail.com>
2023-03-25 16:03:48 -0300

    test: Corrects test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,9 +1,62 @@
 from src.pre_built.brazilian_jobs import read_brazilian_file
-from tests.mocks.formated_brazilians_jobs import formated_brazilians_jobs
 
 
 def test_brazilian_jobs():
+    expected_data = [
+        {"title": "Maquinista", "salary": "2000", "type": "trainee"},
+        {"title": "Motorista", "salary": "3000", "type": "full time"},
+        {
+            "title": "Analista de Software",
+            "salary": "4000",
+            "type": "full time"
+        },
+        {
+            "title": "Assistente administrativo",
+            "salary": "1700",
+            "type": " full time",
+        },
+        {
+            "title": "Auxiliar administrativo",
+            "salary": "1400",
+            "type": " full time",
+        },
+        {"title": "Auxiliar usinagem", "salary": "1400", "type": " full time"},
+        {
+            "title": "Auxiliar de padaria",
+            "salary": "1400",
+            "type": " full time"
+        },
+        {"title": "Analista Contábil", "salary": "1400", "type": " full time"},
+        {"title": "Gerente comercial", "salary": "5000", "type": " full time"},
+        {
+            "title": "Analista de Departamento Pessoal",
+            "salary": "4000",
+            "type": " full time",
+        },
+        {
+            "title": "Esportista de Futebol profissional",
+            "salary": "50000",
+            "type": " full time",
+        },
+        {
+            "title": "Analista de crédito",
+            "salary": "4000",
+            "type": " full time"
+        },
+        {
+            "title": "Pessoa Programadora",
+            "salary": "3000",
+            "type": " full time"
+        },
+        {"title": "Ux Designer", "salary": "3000", "type": " full time"},
+        {
+            "title": "Auxiliar de manutenção",
+            "salary": " 1400",
+            "type": " full time"
+        },
+    ]
+
     path = "tests/mocks/brazilians_jobs.csv"
     actual_data = read_brazilian_file(path)
 
-    assert actual_data == formated_brazilians_jobs
+    assert actual_data == expected_data
