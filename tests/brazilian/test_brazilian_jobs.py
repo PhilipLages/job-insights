085f1ec0ea13241ca819c9b3aea1492592e6012f
Philip Lages <psilvaelages@gmail.com>
2023-03-25 15:52:41 -0300

    test: Implements test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from tests.mocks.formated_brazilians_jobs import formated_brazilians_jobs
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    actual_data = read_brazilian_file(path)
+
+    assert actual_data == formated_brazilians_jobs
