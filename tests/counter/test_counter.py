c3e3c6b287cc00c73a75bead3d0a82a0c52bacbd
Philip Lages <psilvaelages@gmail.com>
2023-03-25 15:23:51 -0300

    test: implements test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    word = "Python"
+    count = count_ocurrences(path, word)
+    assert count == 1639
+
+    word = "Javascript"
+    count = count_ocurrences(path, word)
+    assert count == 122
